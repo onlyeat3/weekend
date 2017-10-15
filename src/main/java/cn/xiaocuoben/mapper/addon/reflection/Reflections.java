3b7e168474343b3990d8028532a9463fb422ac51
onlyeat3 <onlyeat3@gmail.com>
2017-10-15 16:35:58 +0800

    去掉 guava 依赖，使用规范的 Introspector.decapitalize 转换方法名为属性名。

--- src/main/java/cn/xiaocuoben/mapper/addon/reflection/Reflections.java
+++ src/main/java/cn/xiaocuoben/mapper/addon/reflection/Reflections.java
@@ -1,9 +1,8 @@
 package cn.xiaocuoben.mapper.addon.reflection;
 
 import cn.xiaocuoben.mapper.addon.Fn;
-import cn.xiaocuoben.mapper.addon.utils.StringUtils;
-import tk.mybatis.mapper.util.StringUtil;
 
+import java.beans.Introspector;
 import java.lang.invoke.SerializedLambda;
 import java.lang.reflect.Method;
 
@@ -11,15 +10,16 @@ import java.lang.reflect.Method;
  * @author Frank
  */
 public class Reflections {
-    private Reflections(){}
+    private Reflections() {
+    }
 
-    public static String fnToFieldName(Fn fn){
+    public static String fnToFieldName(Fn fn) {
         try {
             Method method = fn.getClass().getDeclaredMethod("writeReplace");
             method.setAccessible(Boolean.TRUE);
             SerializedLambda serializedLambda = (SerializedLambda) method.invoke(fn);
-            String           getter           = serializedLambda.getImplMethodName();
-            String           fieldName        = StringUtils.toCamel(getter.replace("get", ""));
+            String getter = serializedLambda.getImplMethodName();
+            String fieldName = Introspector.decapitalize(getter.replace("get", ""));
             return fieldName;
         } catch (ReflectiveOperationException e) {
             throw new ReflectionOperationException(e);
